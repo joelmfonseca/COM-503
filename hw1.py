f6f3b770de440f852ed5877f257b95f5af8ae975
joelmfonseca <joel.fonseca@epfl.ch>
2019-02-20 20:14:56 +0100

    finished q3

--- hw1.py
+++ hw1.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import numpy as np
 
 def get_values(clients, apoints, servers, sciper=217575):
     '''Return the output values of Joe's Shop Simulator.'''
@@ -49,7 +50,30 @@ def q1_plot_variability(num_sim):
     
     plt.tight_layout()
     fig.savefig('q1_variability.png')
+
+def q3_doubling_access_points(power=4):
+    '''Plot the effect of doubling the number of access points only.'''
+
+    data = []
+    for i in tqdm(range(1,1001)):
+        access_points_data = []
+        for j in range(power):
+            access_points_data.append(get_values(i,1*2**j,1)[0])
+        data.append(access_points_data)
+
+    data = np.array(data)
+
+    fig, ax = plt.subplots(1, 1, figsize=(10,5))
+    for i in range(power):
+        ax.plot(data[:,i], label='AP={}'.format(2**i))
     
+    plt.legend()
+    plt.xlabel('Number of requests per second')
+    plt.ylabel('Theta')
+    plt.tight_layout()
+    fig.savefig('q3_doubling_ap.png')
+
 if __name__ == '__main__':
 
     #q1_plot_variability(num_sim=100)
+    q3_doubling_access_points()
