1978749b501dd190e147f8afc7edd816ff8ea5d9
damachi49 <damachi49>
2019-02-27 15:13:17 +0100

    change code from scatter to line

--- hw1.py
+++ hw1.py
@@ -23,6 +23,8 @@ def get_values(clients, apoints, servers, sciper=217575):
 def q1_plot_variability(num_sim):
     '''Plot variability of output parameters for same input configuration.'''
 
+
+
     list_theta = []
     list_pps = []
     list_prob = []
@@ -73,7 +75,72 @@ def q3_doubling_access_points(power=4):
     plt.tight_layout()
     fig.savefig('q3_doubling_ap.png')
 
+
+def q2_plot_response_values():
+
+    C= 1000
+    AP = 1 
+    S = 1
+
+    theta_data = []
+    pps_data = []
+    col_prob_data  = []
+    delay_data = []
+
+    for clients in range(1,1000):
+        
+        theta, pps, col_prob, delay = get_values(clients, AP, S)
+
+        theta_data.append(theta)
+        pps_data.append(pps)
+        col_prob_data.append(col_prob)
+        delay_data.append(delay)
+
+    fig, ax = plt.subplots(1, 1, figsize=(10,5))
+    fig1, ax1 = plt.subplots(1, 1, figsize=(10,5))
+    fig2, ax2 = plt.subplots(1, 1, figsize=(10,5))
+    fig3, ax3 = plt.subplots(1, 1, figsize=(10,5))
+   
+
+
+    ax.plot(theta_data)
+    ax1.plot(pps_data)
+    ax2.plot(col_prob_data)
+    ax3.plot(delay_data)
+    
+    plt.legend()
+    plt.xlabel('load factor C')
+    plt.ylabel('Theta')
+    plt.tight_layout()
+    fig.savefig('theta.png')
+
+    plt.legend()
+    plt.xlabel('load factor C')
+    plt.ylabel('pps')
+    plt.tight_layout()
+    fig1.savefig('pps.png')
+
+    plt.legend()
+    plt.xlabel('load factor C')
+    plt.ylabel('col_proba')
+    plt.tight_layout()
+    fig2.savefig('col.png')
+
+    plt.legend()
+    plt.xlabel('load factor C')
+    plt.ylabel('delay')
+    plt.tight_layout()
+    fig3.savefig('delay.png')
+
+
+    
+
+
+
+
 if __name__ == '__main__':
 
     #q1_plot_variability(num_sim=100)
     q3_doubling_access_points()
+    #q2_plot_response_values()
+
